5effa63d3bdc2c47fcbf8ccc7abb2abb301d22c4
Audarilanka <dissanayakelanka96@gmail.com>
2022-03-24 18:31:32 +0530

    feat(challenges)

--- java/IfElse.java
+++ java/IfElse.java
@@ -1,5 +1,26 @@
-package java;
+import java.util.*;
 
-public class IfElse {
+
+public class Solution{
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+    
+        if( n%2!=0){
+            System.out.println("Weird");
+        }else if( n>=2 && n<=5){
+            System.out.println("Not Weird");
+        }else if (n>=6 && n<=20){
+            System.out.println("Weird");
+        }else if (n>=20){
+            System.out.println("Not Weird");
+        }
+         in.close();       
+                
+            
+              
+        
+    }
     
 }

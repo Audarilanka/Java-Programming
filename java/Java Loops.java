04061000c7afcbbe669f78a288536b87086b3659
Audarilanka <dissanayakelanka96@gmail.com>
2022-03-28 12:43:24 +0530

    feat(challenges)Java Loops

--- java/Java Loops.java
+++ java/Java Loops.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 
 
-public class JavaLoops {
+public class JavaLoops{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();

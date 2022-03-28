9086a14dadf48ef85a928379f20c1932b2f3ba51
Audarilanka <dissanayakelanka96@gmail.com>
2022-03-28 12:47:27 +0530

    feat(challenges)Java Output Formatting

--- java/JavaOutputFormatting.java
+++ java/JavaOutputFormatting.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class JavaOutputFormatting {
-    
-    
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            //String s1=sc.nextLine();
+            //int x = sc.nextInt();
+            
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s=sc.next();
+                int y=sc.nextInt();
+                System.out.printf("%-15s%03d%n",s,y);
+            }
+            System.out.println("================================");
+
+    }
 }
+
+
+

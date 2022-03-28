0f4fb3e146b6b9e802e64684c14e011be5703615
Audarilanka <dissanayakelanka96@gmail.com>
2022-03-28 12:51:33 +0530

    FEAT(CHALLENGES)Java Stdin and Stdout

--- java/JavaStdinStdouttwo.java
+++ java/JavaStdinStdouttwo.java
@@ -1,3 +1,21 @@
-public class JavaStdinStdouttwo {
+import java .util.*;
+
+public class JavaStdinStdouttwo{
+    public static void main(String[]args){
+    Scanner in = new Scanner(System.in);
+    int x = in.nextInt();
+    Double y = in.nextDouble();
+    String z = in.nextLine();
+    z = in.nextLine();
+    
+    System.out.println("String: " + z);
+    System.out.println("Double: " + y);
+    System.out.println("Int: " + x);
+    
+    }
+    
     
 }
+    
+    
+ 

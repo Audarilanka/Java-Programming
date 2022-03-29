8da757e1dafab8c6f5022d230063a32cd623042a
Audarilanka <dissanayakelanka96@gmail.com>
2022-03-29 11:46:46 +0530

    feat(challenges)JavaLoops

--- java/JavaLoops.java
+++ java/JavaLoops.java
@@ -1,18 +1,15 @@
 import java.util.*;
 import java.io.*;
 
-
-
-
-public class JavaLoops{
+public class JavaLoops {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         in.close();
-        
-        for(int i=1;i<11;i++){
-            int y=n*i;
-            System.out.println(n+" x "+i+" = "+y);
+
+        for (int i = 1; i < 11; i++) {
+            int y = n * i;
+            System.out.println(n + " x " + i + " = " + y);
         }
     }
 }

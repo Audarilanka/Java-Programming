50440f89a74d0bedbacc26b9ba3b98fa0054d6f0
Audarilanka <dissanayakelanka96@gmail.com>
2022-04-14 20:06:50 +0530

    feat(challenges)

--- java/JavaEndOfFile.java
+++ java/JavaEndOfFile.java
@@ -1,17 +1,16 @@
 import java.util.*;
 
-
 public class JavaEndOfFile {
 
     public static void main(String[] args) {
-        
+
         Scanner scan = new Scanner(System.in);
-   int i = 0;
-        while(scan.hasNext()){
-       i++;
-       System.out.println(i + " " + scan.nextLine());
-       scan.close();
-       
-   }
+        int i = 0;
+        while (scan.hasNext()) {
+            i++;
+            System.out.println(i + " " + scan.nextLine());
+            scan.close();
+
+        }
     }
 }

a2354c0e60c187b0f62ffd6862742a7bb24b3d66
Audarilanka <dissanayakelanka96@gmail.com>
2022-04-16 20:10:19 +0530

    feat(challenges)

--- java/javaLoop2.java
+++ java/javaLoop2.java
@@ -1,10 +1,10 @@
 import java.util.*;
-public class JavaLoop2{
- public static void main(String[]args){
-    Scanner in = new Scanner(System.in);
-    in.close();
 
-    
- }
+public class JavaLoop2 {
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      in.close();
+
+   }
 
 }

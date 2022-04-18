ce26df582b47b7bf09d6f91076fbf39bb0290eea
Audarilanka <dissanayakelanka96@gmail.com>
2022-04-19 02:39:51 +0530

    feat(challenges)java

--- java/java/IfElse.java
+++ java/java/IfElse.java
@@ -5,8 +5,7 @@ public class IfElse{
     public static void main(String[]args){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        
-    
+            
         if( n%2!=0){
             System.out.println("Weird");
         }else if( n>=2 && n<=5){

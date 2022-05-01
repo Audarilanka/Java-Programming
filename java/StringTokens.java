63b4cc6be821fbebec7447dbf46a04c1ec88f783
Audarilanka <dissanayakelanka96@gmail.com>
2022-05-01 21:22:20 +0530

    feat(challenges)StringTokens

--- java/StringTokens.java
+++ java/StringTokens.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+public class StringTokens {
     public static void main(String[] args) {
         /* Read input */
         Scanner scan = new Scanner(System.in);

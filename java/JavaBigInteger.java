4dcbabc3725cba14441e64a792120655e7ac4414
Audarilanka <dissanayakelanka96@gmail.com>
2022-05-05 21:43:36 +0530

    JavaBigInteger

--- java/JavaBigInteger.java
+++ java/JavaBigInteger.java
@@ -7,7 +7,7 @@ public class JavaBigInteger {
         Scanner scan = new Scanner(System.in);
         BigInteger a = new BigInteger(scan.nextLine());
         BigInteger b = new BigInteger(scan.nextLine());
-        // BigInteger x=a+b;
+
         System.out.println(a.add(b));
         System.out.println(a.multiply(b));
     }
